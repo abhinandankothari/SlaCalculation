8914d3627ceeac426024849c06987694654e9bf6
Abhinandan Kothari <abhinandankothari@gmail.com>
2012-09-26 18:32:01 +0530

    Changes to UI

---
+++ SlaCalculation/Controllers/CalculateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SlaCalculation.Controllers
+{
+    public class CalculateController : Controller
+    {
+        //
+        // GET: /Calculate/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FinalDisplay()
+        {
+            return View();
+        }
+    }
+}
