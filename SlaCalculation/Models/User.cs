85fe8c5a0161cf05c3ede4d3965551c7a3cc1b43
Abhinandan Kothari <abhinandankothari@gmail.com>
2012-09-26 17:27:50 +0530

    First Commit-Created a New Project and Completed Database Connectivity

---
+++ SlaCalculation/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SlaCalculation.Models
+{
+    public class User
+    {
+    public int ID {get;set;} 
+    public string Name{get;set;}
+    public string EmployeeId { get; set; }
+    public DateTime Dob{get; set;}
+    public string Email{get;set;} 
+    public string Mobile{get;set;}
+    
+    }
+
+    public class UserDBContext:DbContext 
+    {   
+     public DbSet<User> Users {get;set;} 
+     } 
+    
+}
