6847a4a3ce96cc143c6ea8d5849fc30bcd6f2710
Abhinandan Kothari <abhinandankothari@gmail.com>
2012-09-28 14:06:47 +0530

    The Complete Project with all Functionalities

--- SlaCalculation/Controllers/CalculateController.cs
+++ SlaCalculation/Controllers/CalculateController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,117 +11,149 @@ namespace SlaCalculation.Controllers
 {
     public class CalculateController : Controller
     {
+      
         //
         // GET: /Calculate/
         [Authorize(Roles = "NormalUser")]
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-        [Authorize]
-        public ActionResult FinalDisplay()
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
         {
-            try
+            
+            if (file.ContentLength > 0)
             {
-                var myCommand = new OleDbCommand();
-                OleDbDataAdapter Da;
-                var ds = new DataSet();
-                string sql = null;
-                string sql1 = null;
+                var fileName = Path.GetFileName(file.FileName);
+                string fileExtension = Path.GetExtension(file.FileName);
+                string connectionString = "";
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
 
-                int i = 0;
-                var myConnection = new OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source='E:\\Sample.xls';Extended Properties=Excel 8.0;");
-                myConnection.Open();
-                myCommand.Connection = myConnection;
-
-                sql1 = "Select * from [general_report$]";
-                myCommand.CommandText = sql1;
-                Da = new OleDbDataAdapter(myCommand);
-                Da.Fill(ds);
-                while (i < ds.Tables[0].Rows.Count)
+                try
                 {
-                    var rd = Convert.ToDateTime(ds.Tables[0].Rows[i][5].ToString());
-                    var cd = Convert.ToDateTime(ds.Tables[0].Rows[i][4].ToString());
-                    var Time = rd.Subtract(cd);
-                    sql = "Update [general_report$] set Result=" + rd.Hour + "  where [Ticket No]='" + ds.Tables[0].Rows[i][0].ToString() + "'";
-                    myCommand.CommandText = sql;
-                    myCommand.ExecuteNonQuery();
-                    i++;
-                }
-                ds.Clear();
-                myConnection.Close();
+                    var myCommand = new OleDbCommand();
+                    OleDbDataAdapter Da;
+                    var ds = new DataSet();
+                    string sql = null;
+                    string sql1 = null;
 
-
-                myConnection.Open();
-                sql1 = "Select * from [general_report$]";
-                myCommand.CommandText = sql1;
-                Da = new OleDbDataAdapter(myCommand);
-                Da.Fill(ds.Tables[0]);
-                myConnection.Close();
-
-                myConnection.Open();
-
-                i = 0;
-
-                while (i < ds.Tables[0].Rows.Count)
-                {
-                    int d = Convert.ToInt32(ds.Tables[0].Rows[i][6].ToString());
-                    string pri = ds.Tables[0].Rows[i][2].ToString();
-                    switch (pri)
+                    int i = 0;
+                    if (fileExtension == ".xls")
                     {
-                        case "03-Normal Queue":
-                            if (d <= 8)
-                                sql = "Update [general_report$] set Boolean='Yes' where [Ticket No]='" + ds.Tables[0].Rows[i][0].ToString() + "'";
-                            else
-                                sql = "Update [general_report$] set Boolean='No' where [Ticket No]='" + ds.Tables[0].Rows[i][0].ToString() + "'";
 
-                            break;
-                        case "04-Low Priority":
-                            if (d > 8)
-                                sql = "Update [general_report$] set Boolean='Yes' where [Ticket No]='" + ds.Tables[0].Rows[i][0].ToString() + "'";
-                            else
-                                sql = "Update [general_report$] set Boolean='No' where [Ticket No]='" + ds.Tables[0].Rows[i][0].ToString() + "'";
-
-                            break;
-
-                        case "02-Give High Attention":
-                            if (d <= 4)
-                                sql = "Update [general_report$] set Boolean='Yes' where [Ticket No]='" + ds.Tables[0].Rows[i][0].ToString() + "'";
-                            else
-                                sql = "Update [general_report$] set Boolean='No' where [Ticket No]='" + ds.Tables[0].Rows[i][0].ToString() + "'";
-
-                            break;
-
-                        //case default: sql = "Update [general_report$] set Boolean='Undefined Priority' where [Ticket No]='" + Ds.Tables[0].Rows[i][0].ToString() + "'";
+                        connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + path + "';Extended Properties=Excel 8.0;";
                     }
 
-                    i++;
+                    else if (fileExtension == ".xlsx")
+                    {
 
-                    myCommand.CommandText = sql;
-                    myCommand.ExecuteNonQuery();
+                        connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + path + "';Extended Properties=Excel 12.0;";
+                    }
+                    var myConnection = new OleDbConnection(connectionString);
+                    myConnection.Open();
+                    myCommand.Connection = myConnection;
+
+                    sql1 = "Select * from [general_report$]";
+                    myCommand.CommandText = sql1;
+                    Da = new OleDbDataAdapter(myCommand);
+                    Da.Fill(ds);
+                    while (i < ds.Tables[0].Rows.Count)
+                    {
+                        var rd = Convert.ToDateTime(ds.Tables[0].Rows[i][5].ToString());
+                        var cd = Convert.ToDateTime(ds.Tables[0].Rows[i][4].ToString());
+                        var Time = rd.Subtract(cd);
+                        sql = "Update [general_report$] set Result=" + rd.Hour + "  where [Ticket No]='" + ds.Tables[0].Rows[i][0] + "'";
+                        myCommand.CommandText = sql;
+                        myCommand.ExecuteNonQuery();
+                        i++;
+                    }
+                    ds.Clear();
+                    myConnection.Close();
+
+
+                    myConnection.Open();
+                    sql1 = "Select * from [general_report$]";
+                    myCommand.CommandText = sql1;
+                    Da = new OleDbDataAdapter(myCommand);
+                    Da.Fill(ds.Tables[0]);
+                    myConnection.Close();
+
+                    myConnection.Open();
+
+                    i = 0;
+
+                    while (i < ds.Tables[0].Rows.Count)
+                    {
+                        int d = Convert.ToInt32(ds.Tables[0].Rows[i][6].ToString());
+                        string pri = ds.Tables[0].Rows[i][2].ToString();
+                        switch (pri)
+                        {
+                            case "03-Normal Queue":
+                                if (d <= 8)
+                                    sql = "Update [general_report$] set Boolean='Yes' where [Ticket No]='" + ds.Tables[0].Rows[i][0] + "'";
+                                else
+                                    sql = "Update [general_report$] set Boolean='No' where [Ticket No]='" + ds.Tables[0].Rows[i][0]+ "'";
+
+                                break;
+                            case "04-Low Priority":
+                                if (d > 8)
+                                    sql = "Update [general_report$] set Boolean='Yes' where [Ticket No]='" + ds.Tables[0].Rows[i][0] + "'";
+                                else
+                                    sql = "Update [general_report$] set Boolean='No' where [Ticket No]='" + ds.Tables[0].Rows[i][0]+ "'";
+
+                                break;
+
+                            case "02-Give High Attention":
+                                if (d <= 4)
+                                    sql = "Update [general_report$] set Boolean='Yes' where [Ticket No]='" + ds.Tables[0].Rows[i][0] + "'";
+                                else
+                                    sql = "Update [general_report$] set Boolean='No' where [Ticket No]='" + ds.Tables[0].Rows[i][0]+ "'";
+
+                                break;
+
+                            //case default: sql = "Update [general_report$] set Boolean='Undefined Priority' where [Ticket No]='" + Ds.Tables[0].Rows[i][0].ToString() + "'";
+                        }
+
+                        i++;
+
+                        myCommand.CommandText = sql;
+                        myCommand.ExecuteNonQuery();
+
+                    }
+                    ds.Clear();
+                    myConnection.Close();
+                    myConnection.Open();
+                    var sql2 = "Select * from [general_report$] where Boolean='Yes'";
+                    myCommand.CommandText = sql2;
+                    Da = new OleDbDataAdapter(myCommand);
+                    Da.Fill(ds);
+                    int a = ds.Tables[0].Rows.Count;
+                    ViewBag.Message = Convert.ToString(a);
+                    myConnection.Close();
+
+                    myConnection.Open();
+                    sql1 = "Select * from [general_report$]";
+                    myCommand.CommandText = sql1;
+                    Da = new OleDbDataAdapter(myCommand);
+                    Da.Fill(ds);
+                    var model = ds.Tables[0];
+                    myConnection.Close();
+                    return View("Index", model);
 
                 }
-                ds.Clear();
-                myConnection.Close();
 
-                myConnection.Open();
-                sql1 = "Select * from [general_report$]";
-                myCommand.CommandText = sql1;
-                Da = new OleDbDataAdapter(myCommand);
-                Da.Fill(ds);
-                var model = ds.Tables[0];
-                myConnection.Close();
-                return View(model);
+                catch (Exception ex)
+                {
 
+                    ViewBag.Message = ex.ToString();
+                    return Content(ViewBag.Message);
+                }    
             }
-
-            catch (Exception ex)
-            {
-
-                ViewBag.Message = ex.ToString();
-                return Content(ViewBag.Message);
-            }
-         
+            return View();
         }
+       
     }
 }
